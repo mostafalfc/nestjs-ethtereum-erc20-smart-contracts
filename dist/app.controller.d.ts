0bc460aa584e969a16a2824f10d5ca84f10ac83c
Mostafa <67046632+mostafalfc@users.noreply.github.com>
2023-12-26 13:45:43 +0330

    Delete dist directory

--- dist/app.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AppService } from './app.service';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getContractAddress(): string;
-    getTotalSupply(): Promise<number>;
-    getWalletBalance(address: string): Promise<number>;
-    transfer(wallet: string, amount: number): Promise<number>;
-}
--- dist/config/configuration.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: () => {
-    contract_address: string;
-    rpc_url: string;
-    private_key: string;
-};
-export default _default;
